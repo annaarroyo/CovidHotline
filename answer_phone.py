77d8a28ff9650f8b3cff4c441a494c82e77b18d8
kvk745 <kvk745@my.utsa.edu>
2021-02-21 01:27:04 -0600

    finished up

--- answer_phone.py
+++ answer_phone.py
@@ -37,11 +37,16 @@ def checkZip():
         # Get which digit the caller chose
         choice = request.values['Digits']
         resp.say("Here is the zipcode you entered:", voice='alice')
-        resp.say(choice)
+        resp.say(choice[0])
+        resp.say(choice[1])
+        resp.say(choice[2])
+        resp.say(choice[3])
+        resp.say(choice[4])
+
 
         # validate zipcode
         if(zipcodes.is_real(choice)):
-          resp.redirect('/call_number')
+          resp.redirect('/list_places')
         
         resp.say("Zip code not valid.", voice='alice')
         resp.redirect('/answer')
@@ -49,7 +54,7 @@ def checkZip():
     return str(resp)
 
 
-@app.route('/call_number', methods=['GET', 'POST'])
+@app.route('/list_places', methods=['GET', 'POST'])
 def callPhone():
     """Processes results from the <Gather> prompt in /call"""
     # Start our TwiML response
@@ -60,12 +65,48 @@ def callPhone():
     account_sid = os.environ['TWILIO_ACCOUNT_SID']
     auth_token = os.environ['TWILIO_AUTH_TOKEN']
     client = Client(account_sid, auth_token)
-    database.db_fun()
-    resp.dial('512-820-2641')
-    print(resp)
 
+    resp.say("Here are the following places with covid nineteen vaccine appointments: ")
+    resp.say("Wellness 360 Adult Located at 7703 Floyd Curl Dr San Antonio has 7,020 appointments this week.")
+    resp.say("University Health System Inpatient Located at 4502 Medical Dr San Antonio has 12,870 vaccines this week.")
+    resp.say("Samhd Main Immunizations Clinic Located at 210 N Mel Waiters Way San Antonio has 2,340 vaccines this week.")
+
+    gather = Gather(num_digits=1, action='/call_places')
+    gather.say('To be forwarded to a phone line, for Wellness 360 Adult, press 1. For University Health System Inpatient, press 2. For Samhd Main Immunizations Clinic, press 3.')
+    resp.append(gather)
+
+    #database.db_fun()
     return str(resp)
 
+@app.route('/call_places', methods=['GET', 'POST'])
+def call_places():
+    """Processes results from the <Gather> prompt in /voice"""
+    # Start our TwiML response
+    resp = VoiceResponse()
+
+    # If Twilio's request to our app included already gathered digits,
+    # process them
+    if 'Digits' in request.values:
+        # Get which digit the caller chose
+        choice = request.values['Digits']
+
+        # <Say> a different message depending on the caller's choice
+        if choice == '1':
+            resp.say('You selected Wellness 360 Adult. Redirecting your call now...')
+            return str(resp)
+        elif choice == '2':
+            resp.say('You selected University Health System Inpatient. Redirecting your call now...')
+            return str(resp)
+        elif choice == '3':
+            resp.say("You selected Samhd Main Immunizations Clinic. Redirecting your call now...")
+        else:
+            # If the caller didn't choose 1 or 2, apologize and ask them again
+            resp.say("Sorry, I don't understand that choice.")
+
+    # If the user didn't choose 1 or 2 (or anything), send them back to /voice
+    resp.redirect('/list_places')
+
+    return str(resp)
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0',debug=True)
